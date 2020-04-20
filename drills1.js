e4a18bcca3b0adbc61f430ed774c51b2c2dfb138
Carlo Paredes <c.nick.paredes@gmail.com>
2020-04-20 13:57:39 -0700

    Finished drills

--- drills1.js
+++ drills1.js
@@ -1,10 +1,30 @@
 'use strict';
 
-function createGreeting () {
-    return 'Hi, my name is Chris and I\'m 29 years old';
-    
+function createGreeting (name, age) {
+    if(name === undefined || age === undefined) {
+        throw new Error('Arguments not valid');
+    }
 
+    if(age < 0) {
+        throw new Error('Age can not be negative');
+    }
+
+    if(typeof age !== 'number') {
+        throw new TypeError('Age is not a number');
+    }
+
+    const yob = getYearOfBirth(age);
+    return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yob}.`;
 }
-const greeting1 = createGreeting();
-console.log(greeting1);
+
+function getYearOfBirth (age) {
+    return 2020 - age;
+}
+
+try {
+    const greeting1 = createGreeting('Chris', 'twenty nine');
+    console.log(greeting1);
+} catch(err){
+    console.log(err);
+}
 
