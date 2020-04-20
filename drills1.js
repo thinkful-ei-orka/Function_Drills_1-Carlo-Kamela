9136717c5d2400dc31b9e57096514b280b1bb64f
Kamela Williamson <kami63116@yahoo.com>
2020-04-20 13:40:05 -0700

    added changes

--- drills1.js
+++ drills1.js
@@ -1,7 +1,10 @@
 'use strict';
 
 function createGreeting () {
-    console.log('Hi, my name is Chris and I\'m 29 years old');
-}
+    return 'Hi, my name is Chris and I\'m 29 years old';
+    
 
-createGreeting();
+}
+const greeting1 = createGreeting();
+console.log(greeting1);
+
